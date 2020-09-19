b30cfb9baf021443d7f9ca1d675379246fcdd067
rsa1791 <prof.renatao@hotmail.com>
2020-09-19 20:14:54 -0300

    Corrigi um erro retornado pelo Travis build #24.

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,15 @@
+from libpythonpro.tests.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador= Enviador()
+    assert enviador is not None
+def test_remetente():
+    enviador=Enviador():
+    resultado = enviador.enviar(
+        'renzo@pythonpro.br,'
+        'luciano@python.pro.br',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.')
+
+    assert  'renzo@python.pro.br' in resultado
